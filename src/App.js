7584f6d4bb67b63e0204db07042b62af52657e43
Ajay Mittur <ajay.cs18@bmsce.ac.in>
2019-06-20 21:42:49 +0530

    click event handled for all buttons except 'clearAll', sound functionality to be added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from "react"
-import PlayAllButton from "./components/PlayAllButton"
-import TempoButton from "./components/TempoButton"
-import ClearAllButton from "./components/ClearAllButton"
+// import PlayAllButton from "./components/PlayAllButton"
+// import TempoButton from "./components/TempoButton"
+// import ClearAllButton from "./components/ClearAllButton"
+import ControlButtons from "./components/ControlButtons"
 import LoopButton from "./components/LoopButton"
 import "./styles/index.css"
 
 export default function App() {
 	let loop_button_ids = [1, 2, 3, 4]
 	return (
-		<div id="wrapper">
-			<div id="control-buttons">
+		<div id="main-wrapper">
+			<ControlButtons />
+			{/* <div id="control-buttons-wrapper">
 				<PlayAllButton />
 				<TempoButton />
 				<ClearAllButton />
-			</div>
-			<div id="loop-buttons">
+			</div> */}
+			<div id="loop-buttons-wrapper">
 				{loop_button_ids.map(id => (
-					<LoopButton id={id} />
+					<LoopButton key={id} />
 				))}
 			</div>
 		</div>
--- src/components/LoopButton.js
+++ src/components/LoopButton.js
@@ -1,17 +1,62 @@
 import React from "react"
-import "../styles/loopButton.css"
+import loopButtonStyle from "../styles/modules/loopButton.module.css"
 
 class LoopButton extends React.Component {
 	constructor(props) {
 		super(props)
+		this.initialModes = ["Record", "Stop", "Play", "Pause"]
 		this.state = {
-			id: this.props.id
+			style: loopButtonStyle.normal,
+			modes: [...this.initialModes]
 		}
 	}
+
+	handleSingleClick = () => {
+		this.setState(currentState => {
+			// Change previous modes ie. shift them to the left
+			// by 1 position in a circular manner
+			let newModes
+			let currentMode = currentState.modes.shift()
+			if (currentMode !== "Pause") {
+				// Remove 1st element and add it to the end
+				currentState.modes.push(currentMode)
+				newModes = currentState.modes
+			} else {
+				// Remove last element and add it to the beginning
+				let prevMode = currentState.modes.pop()
+				currentState.modes.unshift(currentMode)
+				currentState.modes.unshift(prevMode)
+				newModes = currentState.modes
+			}
+
+			return {
+				id: currentState.id,
+				style:
+					currentState.style === loopButtonStyle.normal
+						? loopButtonStyle.clicked
+						: loopButtonStyle.normal,
+				modes: newModes
+			}
+		})
+	}
+
+	handleDoubleClick = () => {
+		this.setState(currentState => {
+			return {
+				style: loopButtonStyle.normal,
+				modes: [...this.initialModes]
+			}
+		})
+	}
+
 	render() {
 		return (
-			<button type="button" className="loop">
-				{this.state.id}
+			<button
+				type="button"
+				className={this.state.style}
+				onClick={this.handleSingleClick}
+				onDoubleClick={this.handleDoubleClick}>
+				{this.state.modes[0]}
 			</button>
 		)
 	}
