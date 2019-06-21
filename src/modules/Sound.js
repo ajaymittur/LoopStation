dcd2c6d384b4e98b80ad3147ceeb7a0e23b42b93
Ajay Mittur <ajay.cs18@bmsce.ac.in>
2019-06-21 19:56:04 +0530

    Sound module developed and completed

--- src/modules/Sound.js
+++ src/modules/Sound.js
@@ -1,5 +1,44 @@
 class Sound {
-  record() {
-    navigator.
-  }
+	constructor() {
+		this.recorder = null
+		this.audioChunks = []
+		this.audio = new Audio()
+		this.initialize()
+	}
+
+	initialize() {
+		navigator.mediaDevices
+			.getUserMedia({ audio: true })
+			.then(audioStream => {
+				this.recorder = new MediaRecorder(audioStream)
+				this.recorder.ondataavailable = e => {
+					chunks.push(e.data)
+				}
+				this.recorder.onstop = () => {
+					const audioBlob = new Blob(chunks)
+					const audioUrl = URL.createObjectURL(audioBlob)
+					this.audio.src = audioUrl
+					this.audioChunks = []
+				}
+			})
+			.catch(e => console.log(e))
+	}
+
+	record() {
+		this.recorder.start()
+	}
+
+	stop() {
+		this.recorder.stop()
+	}
+
+	play() {
+		this.audio.play()
+	}
+
+	pause() {
+		this.audio.pause()
+	}
 }
+
+export { Sound }
