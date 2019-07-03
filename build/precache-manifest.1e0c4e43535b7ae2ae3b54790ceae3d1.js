fb08d3061af2355dbce8452d9e0272bbefcf7e5c
Ajay Mittur <ajay.cs18@bmsce.ac.in>
2019-07-03 22:09:33 +0530

    Production Build created

---
+++ build/precache-manifest.1e0c4e43535b7ae2ae3b54790ceae3d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff1191cf5b37aaab68e960b08df8e50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d016e1c3799f54601e7e",
+    "url": "/static/css/main.a3d37bbb.chunk.css"
+  },
+  {
+    "revision": "a6e5f817cbb34144ac52",
+    "url": "/static/js/2.88d79664.chunk.js"
+  },
+  {
+    "revision": "d016e1c3799f54601e7e",
+    "url": "/static/js/main.e9535232.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c64361069fbf373d49f4ea88212377d6",
+    "url": "/static/media/tamborine_100bpm.c6436106.mp3"
+  }
+]);
